19c6d54e18df5f03b235a5a408d037917a9b77d6
Sergey Pimenov <sergey@pimenov.com.ua>
2019-03-18 12:18:49 +0200

    minor fix for return valus for empty set

--- build/m4q.js
+++ build/m4q.js
@@ -538,7 +538,7 @@
 	    }
 	}(window));
 
-	var m4qVersion = "v0.1.0 build @@BUILD alpha 18/03/2019 12:18:27";
+	var m4qVersion = "v0.1.0 build 13 alpha 18/03/2019 12:18:44";
 	var regexpSingleTag = /^<([a-z][^\/\0>:\x20\t\r\n\f]*)[\x20\t\r\n\f]*\/?>(?:<\/\1>|)$/i;
 	
 	var matches = Element.prototype.matches
