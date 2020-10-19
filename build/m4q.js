ae9e331b774fb3edb5c533512ad18930f0b96bb9
Sergey Pimenov <sergey@pimenov.com.ua>
2020-10-19 18:36:05 +0300

    release 1.0.9

--- build/m4q.js
+++ build/m4q.js
@@ -621,7 +621,7 @@ function isTouch() {
 
 /* global hasProp */
 
-var m4qVersion = "v1.0.9. Built at 19/10/2020 18:33:58";
+var m4qVersion = "v1.0.9. Built at 19/10/2020 18:36:03";
 
 /* eslint-disable-next-line */
 var matches = Element.prototype.matches
