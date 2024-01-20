f8d0d2ee0bde117d32c045fa571dfd15f1fe9ded
olton <serhii@pimenov.com.ua>
2024-01-20 13:29:01 +0200

    stable 1.1.0

--- build/m4q.js
+++ build/m4q.js
@@ -1,6 +1,6 @@
 /*
- * m4q v1.0.10, (https://github.com/olton/m4q.git)
- * Copyright 2018 - 2021 by Sergey Pimenov
+ * m4q v1.1.0, (https://github.com/olton/m4q.git)
+ * Copyright 2018 - 2024 by Sergey Pimenov
  * Helper for DOM manipulation, animation, and ajax routines.
  * Licensed under MIT
  */
@@ -621,7 +621,7 @@ function isTouch() {
 
 /* global hasProp */
 
-var m4qVersion = "v1.0.10. Built at 08/01/2021 13:03:24";
+var m4qVersion = "v1.1.0. Built at 20/01/2024 13:28:24";
 
 /* eslint-disable-next-line */
 var matches = Element.prototype.matches
