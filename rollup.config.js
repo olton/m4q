6941e3e8cde0622a8b21a77f3fba6b6b87328a02
olton <serhii@pimenov.com.ua>
2024-04-23 15:21:39 +0300

    removed setState because published @olton/hooks

--- rollup.config.js
+++ rollup.config.js
@@ -39,7 +39,6 @@ const source_files = [
     'src/modules/animation.js',
     'src/modules/visibility.js',
     'src/modules/effects.js',
-    'src/modules/state.js',
     'src/modules/init.js',
 ];
 
