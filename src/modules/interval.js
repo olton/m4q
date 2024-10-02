af38572181306d9ecb3c9de0a8b0c88400c5aa4a
olton <serhii@pimenov.com.ua>
2024-10-02 17:48:26 +0300

    impr worker for intervals

--- src/modules/interval.js
+++ src/modules/interval.js
@@ -59,93 +59,92 @@
         return lastFakeId;
     }
 
-    if (typeof (Worker) !== 'undefined') {
-        try {
-            worker = new Worker (workerScript);
-            window.setInterval = function (callback, time /* , parameters */) {
-                const fakeId = getFakeId ();
-                fakeIdToCallback[fakeId] = {
-                    callback: callback,
-                    parameters: Array.prototype.slice.call(arguments, 2)
-                };
+    if (typeof Worker === 'undefined') {
+        return
+    }
+
+    try {
+        worker = new Worker (workerScript);
+        window.setInterval = function (callback, time /* , parameters */) {
+            const fakeId = getFakeId ();
+            fakeIdToCallback[fakeId] = {
+                callback: callback,
+                parameters: Array.prototype.slice.call(arguments, 2)
+            };
+            worker.postMessage ({
+                name: 'setInterval',
+                fakeId: fakeId,
+                time: time
+            });
+            return fakeId;
+        };
+        window.clearInterval = function (fakeId) {
+            if (fakeIdToCallback.hasOwnProperty(fakeId)) {
+                delete fakeIdToCallback[fakeId];
                 worker.postMessage ({
-                    name: 'setInterval',
-                    fakeId: fakeId,
-                    time: time
+                    name: 'clearInterval',
+                    fakeId: fakeId
                 });
-                return fakeId;
+            }
+        };
+        window.setTimeout = function (callback, time /* , parameters */) {
+            const fakeId = getFakeId ();
+            fakeIdToCallback[fakeId] = {
+                callback: callback,
+                parameters: Array.prototype.slice.call(arguments, 2),
+                isTimeout: true
             };
-            window.clearInterval = function (fakeId) {
-                if (fakeIdToCallback.hasOwnProperty(fakeId)) {
-                    delete fakeIdToCallback[fakeId];
-                    worker.postMessage ({
-                        name: 'clearInterval',
-                        fakeId: fakeId
-                    });
-                }
-            };
-            window.setTimeout = function (callback, time /* , parameters */) {
-                const fakeId = getFakeId ();
-                fakeIdToCallback[fakeId] = {
-                    callback: callback,
-                    parameters: Array.prototype.slice.call(arguments, 2),
-                    isTimeout: true
-                };
+            worker.postMessage ({
+                name: 'setTimeout',
+                fakeId: fakeId,
+                time: time
+            });
+            return fakeId;
+        };
+        window.clearTimeout = function (fakeId) {
+            if (fakeIdToCallback.hasOwnProperty(fakeId)) {
+                delete fakeIdToCallback[fakeId];
                 worker.postMessage ({
-                    name: 'setTimeout',
-                    fakeId: fakeId,
-                    time: time
+                    name: 'clearTimeout',
+                    fakeId: fakeId
                 });
-                return fakeId;
-            };
-            window.clearTimeout = function (fakeId) {
-                if (fakeIdToCallback.hasOwnProperty(fakeId)) {
-                    delete fakeIdToCallback[fakeId];
-                    worker.postMessage ({
-                        name: 'clearTimeout',
-                        fakeId: fakeId
-                    });
-                }
-            };
-            window.setImmediate = function (callback) {
-                return setTimeout(callback, 0)
             }
-            window.clearImmediate = function (fakeId) {
-                clearTimeout(fakeId)
-            }
-            worker.onmessage = function (event) {
-                let data = event.data,
-                    fakeId = data.fakeId,
-                    request,
-                    parameters,
-                    callback;
-                if (fakeIdToCallback.hasOwnProperty(fakeId)) {
-                    request = fakeIdToCallback[fakeId];
-                    callback = request.callback;
-                    parameters = request.parameters;
-                    if (request.hasOwnProperty ('isTimeout') && request.isTimeout) {
-                        delete fakeIdToCallback[fakeId];
-                    }
-                }
-                if (typeof (callback) === 'string') {
-                    try {
-                        callback = new Function (callback);
-                    } catch (error) {
-                        console.error (logPrefix + 'Error parsing callback code string: ', error);
-                    }
-                }
-                if (typeof (callback) === 'function') {
-                    callback.apply (window, parameters);
-                }
-            };
-            worker.onerror = function (event) {
-                console.error (event);
-            };
-        } catch (error) {
-            console.error (logPrefix + 'Initialisation failed');
-            console.error (error);
+        };
+        window.setImmediate = function (callback) {
+            return setTimeout(callback, 0)
         }
-    } else {
-        console.error (logPrefix + 'Initialisation failed - HTML5 Web Worker is not supported');
+        window.clearImmediate = function (fakeId) {
+            clearTimeout(fakeId)
+        }
+        worker.onmessage = function (event) {
+            let data = event.data,
+                fakeId = data.fakeId,
+                request,
+                parameters,
+                callback;
+            if (fakeIdToCallback.hasOwnProperty(fakeId)) {
+                request = fakeIdToCallback[fakeId];
+                callback = request.callback;
+                parameters = request.parameters;
+                if (request.hasOwnProperty ('isTimeout') && request.isTimeout) {
+                    delete fakeIdToCallback[fakeId];
+                }
+            }
+            if (typeof (callback) === 'string') {
+                try {
+                    callback = new Function (callback);
+                } catch (error) {
+                    console.error (logPrefix + 'Error parsing callback code string: ', error);
+                }
+            }
+            if (typeof (callback) === 'function') {
+                callback.apply (window, parameters);
+            }
+        };
+        worker.onerror = function (event) {
+            console.error (event);
+        };
+    } catch (error) {
+        console.log (`Can't create worker for Intervals, use standard functions.`);
     }
 })();
