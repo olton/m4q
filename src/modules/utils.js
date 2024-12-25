4ff94a7fc0d3094dc4f6d5fdbda724f5a0276389
Serhii Pimenov <serhii@pimenov.com.ua>
2024-12-25 17:11:59 +0200

    add $.viewport

--- src/modules/utils.js
+++ src/modules/utils.js
@@ -153,6 +153,15 @@ $.extend({
     },
     serialize: function(form){
         return $.serializeToArray(form).join("&");
+    },
+    viewport: function () {
+        const w = window,
+            d = document,
+            e = d.documentElement,
+            g = d.getElementsByTagName('body')[0],
+            x = w.innerWidth || e.clientWidth || g.clientWidth,
+            y = w.innerHeight || e.clientHeight || g.clientHeight;
+        return {width: x, height: y};
     }
 });
 
